b51aa3fbffda3190f4f6d35be8e526996ea4c4cb
bear <istoeumteste2022@gmail.com>
2022-02-12 23:17:13 +0000

    lancamento

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Remember - v4.7.0
-* Template URL: https://bootstrapmade.com/remember-free-multipurpose-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
